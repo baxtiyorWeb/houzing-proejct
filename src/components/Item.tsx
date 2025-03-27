7be73815b91b64b7544a9c110bca22a7e0e9f6da
baxtiyorWeb <baxtiyorqurbonnazarov33@gmail.com>
2025-03-27 16:05:59 +0500

    fix

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { BathIcon, BedIcon } from '@/assets/houzing-images'
 import userImg from "@/assets/user.png"
